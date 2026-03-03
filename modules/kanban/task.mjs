2823b60a2a9db76cbe8f425eba34121b2ebce216
Yuna Emiya <yuna-emiya@outlook.com>
2026-03-03 03:19:50 +0100

    Added KanbanTask class

---
+++ modules/kanban/task.mjs
@@ -0,0 +1,8 @@
+export class KanbanTask {
+  constructor(id, title, category, originListId) {
+    this.id = id;
+    this.title = title;
+    this.originalCategory = category;
+    this.originListId = originListId;
+  }
+}

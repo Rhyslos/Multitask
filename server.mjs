891cbefb38b7f31a223a9f06fca4903ee5241398
Yuna Emiya <yuna-emiya@outlook.com>
2026-03-05 22:29:30 +0100

    Added modules to let the server execute the files

--- server.mjs
+++ server.mjs
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 8080;
 // Middleware functions
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/modules', express.static('modules'));
 
 // Server execution functions
 app.listen(PORT, () => {

ac4ba677c56a432d0fed3c329c0cfe82a49dce14
Yuna Emiya <yuna-emiya@outlook.com>
2026-03-21 13:37:57 +0100

    Updated KanbanColumn with AnimatedRemoval, stable column CSS var and list element registration

--- client/src/components/kanban/KanbanColumn.jsx
+++ client/src/components/kanban/KanbanColumn.jsx
@@ -1,3 +1,4 @@
+import AnimatedRemoval from '../AnimatedRemoval';
 import KanbanList from './KanbanList';
 
 
@@ -5,30 +6,34 @@ import KanbanList from './KanbanList';
 export default function KanbanColumn({
     colIndex, lists, tasks, categories, focusedListId, dragging, insertionPoint, isDragging,
     onUpdateList, onDeleteList, onAddTask, onUpdateTask, onDeleteTask,
-    onStartDrag, onOpenTask, onFocusClear, registerList, registerTask, registerGhost
+    onStartDrag, onOpenTask, onFocusClear, registerList, registerTask,
+    registerGhost, registerTaskElement, registerListElement, removingIds
 }) {
     return (
-        <div className="kanban-column">
+        <div className="kanban-column" style={{ '--col': colIndex }}>
             {lists.map(list => (
-                <KanbanList
-                    key={list.id}
-                    list={list}
-                    tasks={tasks.filter(t => t.listID === list.id).sort((a, b) => a.taskOrder - b.taskOrder)}
-                    categories={categories}
-                    isFocused={focusedListId === list.id}
-                    dragging={dragging}
-                    insertionPoint={insertionPoint}
-                    onUpdate={changes => onUpdateList(list.id, changes)}
-                    onDelete={() => onDeleteList(list.id)}
-                    onAddTask={() => onAddTask(list.id)}
-                    onUpdateTask={onUpdateTask}
-                    onDeleteTask={onDeleteTask}
-                    onStartDrag={onStartDrag}
-                    onOpenTask={onOpenTask}
-                    onFocusClear={onFocusClear}
-                    registerList={registerList}
-                    registerTask={registerTask}
-                />
+                <AnimatedRemoval key={list.id} removing={removingIds?.has(list.id)}>
+                    <KanbanList
+                        list={list}
+                        tasks={tasks.filter(t => t.listID === list.id).sort((a, b) => a.taskOrder - b.taskOrder)}
+                        categories={categories}
+                        isFocused={focusedListId === list.id}
+                        dragging={dragging}
+                        insertionPoint={insertionPoint}
+                        onUpdate={changes => onUpdateList(list.id, changes)}
+                        onDelete={() => onDeleteList(list.id)}
+                        onAddTask={() => onAddTask(list.id)}
+                        onUpdateTask={onUpdateTask}
+                        onDeleteTask={onDeleteTask}
+                        onStartDrag={onStartDrag}
+                        onOpenTask={onOpenTask}
+                        onFocusClear={onFocusClear}
+                        registerList={registerList}
+                        registerTask={registerTask}
+                        registerTaskElement={registerTaskElement}
+                        registerListElement={registerListElement}
+                    />
+                </AnimatedRemoval>
             ))}
 
             {isDragging && (
